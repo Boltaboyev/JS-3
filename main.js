729224900b1e4a43a3d02d5c3968e3e5ad3af0f6
Otabekcdr <boltaboyevotabek207@gmail.com>
2024-11-30 14:56:03 +0500

    js

--- main.js
+++ main.js
@@ -6,22 +6,19 @@
 // res = str.split(" ").reverse().join(" ")
 // console.log(res);
 
-
 // 2 ------------------------------------------------------------------2
 // let str  = 'level'
 // let res = ''
 
-// for (let i = str.length - 1; i >= 0; i--) {  
-//     res += str[i]    
+// for (let i = str.length - 1; i >= 0; i--) {
+//     res += str[i]
 // }
 
 // if (res === str) {
 //     console.log(true);
 // }else {
-//     console.log(false);    
+//     console.log(false);
 // }
-
-
 
 // 3 -----------------------------------------------------------------3
 // let str = 'HellO'
@@ -29,11 +26,19 @@
 
 // for (let i = 0; i < str.length; i++) {
 //     if (str[i] === str[i].toUpperCase()) {
-//         res++        
-//     }        
+//         res++
+//     }
 // }
 
 // console.log(res);
 
-
 // 4 -----------------------------------------------------------------4
+// let str = "abc"
+// let res = ""
+// let res2 = ""
+
+// for (let i = 0; i < str.length; i++) {
+//     res2 += String.fromCharCode(str[i].charCodeAt()+1)
+// }
+
+// console.log(res2)
